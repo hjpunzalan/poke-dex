0e5230f28e0a1d42ddc5f1bfe614034a17713ce0
hjpunzalan <47600145+hjpunzalan@users.noreply.github.com>
2019-05-12 00:32:28 +0800

    Updates

---
+++ precache-manifest.7018c05ad8984079df0e408a8870f9c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a63adbd3a385eada65632620de51049a",
+    "url": "/poke-dex/index.html"
+  },
+  {
+    "revision": "7f4593eca1237e75eb5f",
+    "url": "/poke-dex/static/css/main.7255d1e8.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/poke-dex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "7f4593eca1237e75eb5f",
+    "url": "/poke-dex/static/js/main.5f07db6a.chunk.js"
+  },
+  {
+    "revision": "54c5d82ee5c3c62038e7",
+    "url": "/poke-dex/static/js/runtime~main.878edc27.js"
+  }
+]);
